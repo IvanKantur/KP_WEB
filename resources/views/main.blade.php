348a199454c4a5da8c7eb235cb6a6b7d16424ca4
Ivan Kantur <ivankantur87@gmail.com>
2026-06-02 13:15:02 +0300

    fix css path using url helper

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TechStore - @yield('title', 'Интернет-магазин компьютерной техники')</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
+    <link rel="stylesheet" href="{{ url('/css/style.css') }}"> //для опубликованной
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
